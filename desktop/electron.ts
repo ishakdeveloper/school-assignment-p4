0bdb06b64971e3bec4933bb92a2ffaa4913e49e4
Ishak Kahrimanovic <ishak.business@outlook.com>
2021-06-25 18:26:37 +0200

    remove unused functions

--- desktop/electron.ts
+++ desktop/electron.ts
@@ -1,43 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 
-function createSplashWindow() {
-    const splash = new BrowserWindow({
-        width: 800,
-        height: 800,
-        transparent: true,
-        frame: false,
-        resizable: false,
-        webPreferences: {
-          nodeIntegration: true,
-          contextIsolation: false,
-        },
-    })
-
-    splash.loadFile(path.join(__dirname, 'resources/splash-screen.html'))
-}
-
-function createMainWindow() {
-    const win = new BrowserWindow({
-        width: 1500,
-        height: 800,
-        minWidth: 400,
-        minHeight: 600,
-        autoHideMenuBar: true,
-        show: false,
-        webPreferences: {
-          nodeIntegration: true,
-          contextIsolation: false,
-        },
-    })
-
-    win.loadUrl('http://localhost:3000/')
-
-    win.once('ready-to-show', () => {
-        win.show()
-    })
-}
-
 app.whenReady().then(() => {
     const splash = new BrowserWindow({
         width: 800,
@@ -73,9 +36,6 @@ app.whenReady().then(() => {
     })
 })
 
-app.on('activate', () => {
-    createMainWindow()
-})
 
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
