8830ff3e461f4d007e0c98ea4d8497024d644d2e
Ishak Kahrimanovic <ishak.business@outlook.com>
2021-06-25 18:25:39 +0200

    Splash screen and load main URL

---
+++ desktop/electron.ts
@@ -0,0 +1,83 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+function createSplashWindow() {
+    const splash = new BrowserWindow({
+        width: 800,
+        height: 800,
+        transparent: true,
+        frame: false,
+        resizable: false,
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false,
+        },
+    })
+
+    splash.loadFile(path.join(__dirname, 'resources/splash-screen.html'))
+}
+
+function createMainWindow() {
+    const win = new BrowserWindow({
+        width: 1500,
+        height: 800,
+        minWidth: 400,
+        minHeight: 600,
+        autoHideMenuBar: true,
+        show: false,
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false,
+        },
+    })
+
+    win.loadUrl('http://localhost:3000/')
+
+    win.once('ready-to-show', () => {
+        win.show()
+    })
+}
+
+app.whenReady().then(() => {
+    const splash = new BrowserWindow({
+        width: 800,
+        height: 800,
+        transparent: true,
+        frame: false,
+        resizable: false,
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false,
+        },
+    })
+
+    const win = new BrowserWindow({
+        width: 1500,
+        height: 800,
+        minWidth: 400,
+        minHeight: 600,
+        autoHideMenuBar: true,
+        show: false,
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false,
+        },
+    })
+
+    splash.loadFile(path.join(__dirname, 'resources/splash-screen.html'))
+    win.loadURL('http://localhost:3000/')
+
+    win.once('ready-to-show', () => {
+        splash.destroy()
+        win.show()
+    })
+})
+
+app.on('activate', () => {
+    createMainWindow()
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
+
